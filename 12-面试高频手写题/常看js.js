e8579023ab7f879e0b2c286d2d0a3d927ee35401
wenxint <example@example.com>
2025-05-27 14:47:44 +0800

    feat: 添加查找最长不重复子串长度的函数
    实现滑动窗口算法来计算字符串中最长不重复字符子串的长度，示例输入"abcabcbb"将返回3

--- 12-面试高频手写题/常看js.js
+++ 12-面试高频手写题/常看js.js
@@ -124,4 +124,25 @@ function mySetInterval(callback, delay) {
 // 使用示例
 mySetInterval(() => console.log("Hello, world!"), 1000);
 
+//要找到字符串中出现的不重复字符的最长长度
+
+function lengthOfLongestSubstring(s) {
+  let start = 0; // 窗口起始位置
+  let maxLength = 0; // 最长不重复子串的长度
+  let seen = new Set(); // 用于存储窗口内的字符
+  for (let end = 0; end < s.length; end++) {
+    // 如果当前字符已经在窗口内，则移动窗口的起始位置
+    while (seen.has(s[end])) {
+      seen.delete(s[start]);
+      start++;
+    } // 将当前字符添加到窗口内
+    seen.add(s[end]); // 更新最长不重复子串的长度
+    maxLength = Math.max(maxLength, end - start + 1);
+  }
+  return maxLength;
+}
+// 示例
+const s = "abcabcbb";
+console.log(lengthOfLongestSubstring(s)); // 输出 3
+
 // https://juejin.cn/post/7353456468094599205?searchId=202505271123555EE696FDB0964BA59F47#heading-28
